19180d0e23fff94ce5994c056654257964a670d9
Cookie <eg.melnikk2@gmail.com>
2025-01-08 02:31:30 +0300

    feat: added ui button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import { FC } from "react";
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/ui/Button";
 
 const App: FC = () => {
-	return <h1 className="text-3xl font-bold underline">Test</h1>;
+	return (
+		<>
+			<h1 className="text-3xl font-bold underline">Test</h1>
+
+			<div
+				className="pt-[4.75rem] lg:pt-[5.25rem] 
+			overflow-hidden"
+			>
+				<Button className="mt-10">Something</Button>
+			</div>
+			<ButtonGradient />
+		</>
+	);
 };
 
 export default App;
---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,47 @@
+import { FC, MouseEventHandler } from "react";
+import ButtonSvg from "../../assets/svg/ButtonSvg";
+
+interface ButtonProps {
+	className: string;
+	children: React.ReactNode;
+	href?: string;
+	onClick?: MouseEventHandler<HTMLButtonElement>;
+	px?: string;
+	isWhite?: boolean;
+}
+
+const Button: FC<ButtonProps> = ({
+	className,
+	href,
+	onClick,
+	children,
+	px,
+	isWhite = false,
+}) => {
+	const classes = `button relative inline-flex items-center 
+	justify-center h-11 transition-colors hover:text-color-1 
+	${px || "px-6"} ${isWhite ? "text-n-8" : "text-n-1"}
+	${className || ""}`;
+	const spanClasses = `relative z-10`;
+
+	const renderButton = () => (
+		<button className={classes}>
+			<span className={spanClasses}>{children}</span>
+			{ButtonSvg(isWhite)}
+		</button>
+	);
+
+	const renderLink = () => (
+		<a
+			className={classes}
+			href={href}
+		>
+			<span className={spanClasses}>{children}</span>
+			{ButtonSvg(isWhite)}
+		</a>
+	);
+
+	return href ? renderLink() : renderButton();
+};
+
+export default Button;
